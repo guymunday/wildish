aa8b36ab98cfb4546c904d0641696ca76fe5cd7b
guymunday <guymunday@gmail.com>
2021-05-28 17:36:55 +0100

    fix bug

--- src/components/case-studies/slices/RollIn.js
+++ src/components/case-studies/slices/RollIn.js
@@ -1,39 +1,40 @@
 import * as React from "react";
 import styled from "styled-components";
-import { gsap } from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
+import FadeIn from "../../FadeIn";
+// import { gsap } from "gsap";
+// import { ScrollTrigger } from "gsap/ScrollTrigger";
 import Image from "../../Image";
 import Copy from "./Copy";
 
-gsap.registerPlugin(ScrollTrigger);
+// gsap.registerPlugin(ScrollTrigger);
 
-const RollInStyles = styled.section`
-  width: 100%;
-  position: relative;
-  @media screen and (max-width: 768px) {
-    display: none;
-  }
-  .rollin-image {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-    z-index: 0;
-    position: relative;
-    .alt-hover-image {
-      position: absolute;
-    }
-  }
-  .rollin-copy {
-    position: absolute;
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0;
-    display: flex;
-    align-items: center;
-    background: var(--white);
-  }
-`;
+// const RollInStyles = styled.section`
+//   width: 100%;
+//   position: relative;
+//   @media screen and (max-width: 768px) {
+//     display: none;
+//   }
+//   .rollin-image {
+//     min-height: 100vh;
+//     display: flex;
+//     align-items: center;
+//     z-index: 0;
+//     position: relative;
+//     .alt-hover-image {
+//       position: absolute;
+//     }
+//   }
+//   .rollin-copy {
+//     position: absolute;
+//     top: 0;
+//     left: 0;
+//     right: 0;
+//     bottom: 0;
+//     display: flex;
+//     align-items: center;
+//     background: var(--white);
+//   }
+// `;
 
 const RollInStylesMobile = styled.section`
   width: 100%;
@@ -53,24 +54,24 @@ const RollInStylesMobile = styled.section`
 `;
 
 export default function RollIn({ alt, input: { image, text } }) {
-  const containerRef = React.useRef(null);
-  const imageRef = React.useRef(null);
-  const copyRef = React.useRef(null);
+  // const containerRef = React.useRef(null);
+  // const imageRef = React.useRef(null);
+  // const copyRef = React.useRef(null);
 
-  React.useEffect(() => {
-    let tl = gsap.timeline({
-      scrollTrigger: {
-        trigger: containerRef.current,
-        pin: true,
-        start: "top top",
-        end: "+=500",
-        scrub: 1,
-      },
-    });
-    tl.from(copyRef.current, {
-      yPercent: 100,
-    });
-  }, []);
+  // React.useEffect(() => {
+  //   let tl = gsap.timeline({
+  //     scrollTrigger: {
+  //       trigger: containerRef.current,
+  //       pin: true,
+  //       start: "top top",
+  //       end: "+=500",
+  //       scrub: 1,
+  //     },
+  //   });
+  //   tl.from(copyRef.current, {
+  //     yPercent: 100,
+  //   });
+  // }, []);
 
   return (
     <>
@@ -87,12 +88,16 @@ export default function RollIn({ alt, input: { image, text } }) {
         </div>
       </RollInStyles> */}
       <RollInStylesMobile>
-        <div className="rollin-image">
-          {alt ? <Copy html={text} /> : <Image image={image} />}
-        </div>
-        <div className="rollin-copy">
-          {alt ? <Image image={image} /> : <Copy html={text} />}
-        </div>
+        <FadeIn>
+          <div className="rollin-image">
+            {alt ? <Copy html={text} /> : <Image image={image} />}
+          </div>
+        </FadeIn>
+        <FadeIn>
+          <div className="rollin-copy">
+            {alt ? <Image image={image} /> : <Copy html={text} />}
+          </div>
+        </FadeIn>
       </RollInStylesMobile>
     </>
   );
