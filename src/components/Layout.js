3a3e759c6ba63029ad083d84b98b7b7304cf3ef0
guymunday <guymunday@gmail.com>
2021-08-02 10:04:33 +0100

    SEO to layout

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -14,7 +14,8 @@ import MenuFooter from "./MenuFooter"
 import IntroScreen from "./IntroScreen"
 import { Helmet } from "react-helmet"
 import favicon from "../assets/favICON.png"
-import CallToAction from "./CallToAction"
+import { useStaticQuery, graphql } from "gatsby"
+import { SEOContext } from "gatsby-plugin-wpgraphql-seo"
 
 const GlobalStyle = createGlobalStyle`
   ${reset}
@@ -23,26 +24,104 @@ const GlobalStyle = createGlobalStyle`
 
 const Layout = ({ location, children }) => {
   const [menuOpen, setMenuOpen] = React.useState(false)
-  
+
+  const {
+    wp: { seo },
+  } = useStaticQuery(graphql`
+    query SiteInfoQuery {
+      wp {
+        seo {
+          contentTypes {
+            post {
+              title
+              schemaType
+              metaRobotsNoindex
+              metaDesc
+            }
+            page {
+              metaDesc
+              metaRobotsNoindex
+              schemaType
+              title
+            }
+          }
+          webmaster {
+            googleVerify
+            yandexVerify
+            msVerify
+            baiduVerify
+          }
+          schema {
+            companyName
+            personName
+            companyOrPerson
+            wordpressSiteName
+            siteUrl
+            siteName
+            inLanguage
+            logo {
+              sourceUrl
+              mediaItemUrl
+              altText
+            }
+          }
+          social {
+            facebook {
+              url
+              defaultImage {
+                sourceUrl
+                mediaItemUrl
+              }
+            }
+            instagram {
+              url
+            }
+            linkedIn {
+              url
+            }
+            mySpace {
+              url
+            }
+            pinterest {
+              url
+              metaTag
+            }
+            twitter {
+              username
+            }
+            wikipedia {
+              url
+            }
+            youTube {
+              url
+            }
+          }
+        }
+      }
+    }
+  `)
+
   return (
     <>
-      <GlobalStyle />
-      <IntroScreen />
-      <Helmet>
-        <html lang="en" />
-        <link rel="icon" type="image/png" href={favicon} />
-        <link rel="canonical" href={location.href} />
-      </Helmet>
-      <Transition location={location}>{children}</Transition>
-      {menuOpen && <Menu setMenuOpen={setMenuOpen} menuOpen={menuOpen} />}
-      <Hamburger setMenuOpen={setMenuOpen} />
-      {!location.pathname.includes("/client/") && (
-        <Logo setMenuOpen={setMenuOpen} />
-      )}
-      <MenuFooter />
-      <Footer />
-      <CookiesBanner />
-      {!location.pathname.includes("/client/") && <Cursor />}
+      <SEOContext.Provider value={{ global: seo }}>
+        <GlobalStyle />
+        <IntroScreen />
+        <Helmet>
+          <html lang="en" />
+          <link rel="icon" type="image/png" href={favicon} />
+          <link rel="canonical" href={location.href} />
+        </Helmet>
+        <Transition location={location}>{children}</Transition>
+        {menuOpen && <Menu setMenuOpen={setMenuOpen} menuOpen={menuOpen} />}
+        <Hamburger setMenuOpen={setMenuOpen} />
+        {!location.pathname.includes("/client/") && (
+          <Logo setMenuOpen={setMenuOpen} />
+        )}
+        <MenuFooter />
+        <Footer />
+        <CookiesBanner />
+        {!location.pathname.includes("/client/") && <Cursor />}
+      </SEOContext.Provider>
     </>
   )
 }
